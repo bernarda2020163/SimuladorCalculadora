839a13188e7e395f22e3b280642e8867a3580693
Romina Parra <parra.acevedo.romina@gmail.com>
2025-09-22 11:57:00 -0300

    Agrego archivo con cambio de procedimientos a funciones de Java

--- src/main/java/latinasincloud/Main.java
+++ src/main/java/latinasincloud/Main.java
@@ -26,8 +26,8 @@ public class Main {
 
     // declarar la variable publica en el Main
 
-    private static Scanner scanner = new Scanner(System.in); // Creamos un objeto Scanner para leer la entrada del usuario
-    private static List<Double> numeros = new ArrayList<>(); // Creamos una lista para almacenar los números de entrada de la suma multiple
+    public static Scanner scanner = new Scanner(System.in); // Creamos un objeto Scanner para leer la entrada del usuario
+    public static List<Double> numeros = new ArrayList<>(); // Creamos una lista para almacenar los números de entrada de la suma multiple
 
 
     public static void main(String[] args) {
@@ -47,18 +47,92 @@ public class Main {
             scanner.nextLine();
             // Detalle de la opcion del ménu que eligió el usuario
             switch (opcion){
-                case 1:
-                    sumar(); //procedimiento
-                    break;
+                case 1://////
+                    System.out.println("Usted a escogido la operación sumar dos números");
+                    //solicitar el primer número
+                    System.out.println("Indique el primer valor de la suma :");
+                    double valor_uno = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("Indique el segundo valor de la suma : ");
+                    double valor_dos = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    double suma = sumar(valor_uno, valor_dos); //función
+                    System.out.println("El valor de la suma es : " + suma);
+                    System.out.println("--------------------------------------------");
+
+                    break;//////
                 case 2:
-                    sumamultiple(); //procedimiento
+                    // Variable para controlar si el usuario quiere seguir añadiendo números
+                    boolean continueAdding = true;
+
+                    System.out.println("Programa para sumar múltiples valores.");
+                    System.out.println("Ingresa 'fin' para terminar de agregar números.");
+
+                    // Bucle para leer los números del usuario hasta que ingrese 'fin'
+                    while (continueAdding) {
+                        System.out.print("Ingresa un número (o 'fin' para sumar): ");
+                        String input = scanner.nextLine();
+
+                        // Verificamos si la entrada es "fin"
+                        if (input.equalsIgnoreCase("fin")) {
+                            continueAdding = false;
+                        } else {
+                            try {
+                                // Convertimos la entrada a un tipo de dato Double
+                                double number = Double.parseDouble(input);
+                                // Agregamos el número a la lista
+                                numeros.add(number);
+                            } catch (NumberFormatException e) {
+                                // Manejamos el error si la entrada no es un número
+                                System.out.println("Entrada inválida. Por favor, ingresa un número válido o 'fin'.");
+                            }
+                        }
+                    }
+                    /* // Cerramos el Scanner para liberar recursos
+                    scanner.close(); */
+                    double res = sumamultiple(numeros); //procedimiento
                     break;
                 case 3:
-                    restar();
+                    System.out.println("Usted a escogido la operación restar dos números");
+                    //solicitar el primer número
+                    System.out.println("Indique el primer valor(minuendo) de la resta :");
+                    double valor_uno1 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("Indique el segundo valor(sustraendo) de la resta : ");
+                    double valor_dos1 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("\n--- Resultado ---");
+                    double resta = restar(valor_uno1, valor_dos1);
+                    System.out.println("El valor de la resta es : " + resta);
+                    System.out.println("--------------------------------------------");
                     break;
-                case 4: multiplicar();
+                case 4:
+                    System.out.println("Usted a escogido la operación multiplicar dos números");
+                    //solicitar el primer número
+                    System.out.println("Indique el primer valor de la multiplicación :");
+                    double valor_uno2 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("Indique el segundo valor de la multiplicación : ");
+                    double valor_dos2 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("\n--- Resultado ---");
+                    double multiplicacion = multiplicar(valor_uno2, valor_dos2);
+                    System.out.println("El valor de la multiplicación es : " + multiplicacion);
+                    System.out.println("--------------------------------------------");
                     break;
-                case 5: dividir();
+                case 5:
+                    System.out.println("Usted ha escogido la operación dividir dos números");
+                    //solicitar el primer número
+                    System.out.println("Indique el primer valor de la división :");
+                    double valor_uno3 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("Indique el segundo valor de la division : ");
+                    double valor_dos3 = scanner.nextDouble();
+                    //scanner.nextLine();// limpiar buffer
+                    System.out.println("\n--- Resultado ---");
+                    double division = dividir(valor_uno3, valor_dos3);
+                    System.out.println("El valor de la división es : " + division);
+                    System.out.println("--------------------------------------------");
                     break;
                 case 6:
                     System.out.println("Gracias por usar la calculadora de operaciones básicas, Adios!");
@@ -70,48 +144,12 @@ public class Main {
     }
     // fuera del Main agrego los procedimientos por que no retornan valor.
 
-    private static void sumar() {
-        System.out.println("Usted a escogido la operación sumar dos números");
-        //solicitar el primer número
-        System.out.println("Indique el primer valor de la suma :");
-        double valor_uno = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        System.out.println("Indique el segundo valor de la suma : ");
-        double valor_dos = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        double suma = valor_uno + valor_dos;
-        System.out.println("El valor de la suma es : " + suma);
-        System.out.println("--------------------------------------------");
+/// ///
+    public static double sumar(double valor_uno, double valor_dos){
+        return valor_uno + valor_dos;
     }
-    private static void sumamultiple() {
-        // Variable para controlar si el usuario quiere seguir añadiendo números
-        boolean continueAdding = true;
-
-        System.out.println("Programa para sumar múltiples valores.");
-        System.out.println("Ingresa 'fin' para terminar de agregar números.");
-
-        // Bucle para leer los números del usuario hasta que ingrese 'fin'
-        while (continueAdding) {
-            System.out.print("Ingresa un número (o 'fin' para sumar): ");
-            String input = scanner.nextLine();
-
-            // Verificamos si la entrada es "fin"
-            if (input.equalsIgnoreCase("fin")) {
-                continueAdding = false;
-            } else {
-                try {
-                    // Convertimos la entrada a un tipo de dato Double
-                    double number = Double.parseDouble(input);
-                    // Agregamos el número a la lista
-                    numeros.add(number);
-                } catch (NumberFormatException e) {
-                    // Manejamos el error si la entrada no es un número
-                    System.out.println("Entrada inválida. Por favor, ingresa un número válido o 'fin'.");
-                }
-            }
-        }
-        /* // Cerramos el Scanner para liberar recursos
-        scanner.close(); */
+/// ///
+    public static double sumamultiple(List<Double> numeros) {
 
         // Verificamos si la lista no está vacía antes de procesar
         if (!numeros.isEmpty()) {
@@ -122,7 +160,7 @@ public class Main {
 
             // Calculamos la suma de los valores en la lista
             double sum = 0;
-            for (Double num : numeros) {
+            for (double num : numeros) {
                 sum += num;
             }
 
@@ -137,61 +175,23 @@ public class Main {
         } else {
             System.out.println("\nNo se ingresaron números para sumar.");
         }
+        return 0;
     }
-
-    private static void restar() {
-        System.out.println("Usted a escogido la operación restar dos números");
-        //solicitar el primer número
-        System.out.println("Indique el primer valor(minuendo) de la resta :");
-        double valor_uno = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        System.out.println("Indique el segundo valor(sustraendo) de la resta : ");
-        double valor_dos = scanner.nextDouble();
-
-        //scanner.nextLine();// limpiar buffer
-        double resta = valor_uno - valor_dos;
-        System.out.println("\n--- Resultado ---");
-        System.out.println("El valor de la resta es : " + resta);
-        System.out.println("--------------------------------------------");
+    public static double restar(double valor_uno, double valor_dos) {
+        return valor_uno - valor_dos;
     }
-
-    private static void multiplicar() {
-        System.out.println("Usted a escogido la operación multiplicar dos números");
-        //solicitar el primer número
-        System.out.println("Indique el primer valor de la multiplicación :");
-        double valor_uno = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        System.out.println("Indique el segundo valor de la multiplicación : ");
-        double valor_dos = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        double multiplicacion = valor_uno * valor_dos;
-        System.out.println("\n--- Resultado ---");
-        System.out.println("El valor de la multiplicación es : " + multiplicacion);
-        System.out.println("--------------------------------------------");
+    public static double multiplicar(double valor_uno, double valor_dos) {
+        return valor_uno * valor_dos;
     }
-    private static void dividir() {
-        System.out.println("Usted ha escogido la operación dividir dos números");
-        //solicitar el primer número
-        System.out.println("Indique el primer valor de la división :");
-        double valor_uno = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-        System.out.println("Indique el segundo valor de la division : ");
-        double valor_dos = scanner.nextDouble();
-        //scanner.nextLine();// limpiar buffer
-
+    private static double dividir(double valor_uno, double valor_dos) {
         // Condición de error para el valor_dos cuando es cero
         if (valor_dos == 0) {
             System.out.println("Error: No es posible dividir por cero.");
         } else {
-            double division = valor_uno / valor_dos;
-            System.out.println("\n--- Resultado ---");
-            System.out.println("El valor de la división es : " + division);
+            return valor_uno / valor_dos;
         }
-
-        System.out.println("--------------------------------------------");
+        return 0;
     }
-
-
     }
 
 
